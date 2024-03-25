06a6757a5c66b4e0946acf87d134c2ec592c1afd
김보현 <qhgus5459@naver.com>
2024-03-25 09:21:24 +0900

    Croaks FireFly 구현 중

--- GameObject/Bullet/BulletCroaksFirefly.cpp
+++ GameObject/Bullet/BulletCroaksFirefly.cpp
@@ -1,5 +1,7 @@
 #include "pch.h"
 #include "BulletCroaksFirefly.h"
+#include "SceneDev3.h"
+#include "SceneGame.h"
 
 BulletCroaksFirefly::BulletCroaksFirefly(const std::string& name)
 	:ObjectBullet(name)
@@ -8,17 +10,27 @@ BulletCroaksFirefly::BulletCroaksFirefly(const std::string& name)
 
 BulletCroaksFirefly* BulletCroaksFirefly::Create(const sf::Vector2f& pos, const sf::Vector2f& direction, Scene* scene)
 {
-	return nullptr;
+	BulletCroaksFirefly* bcf = new BulletCroaksFirefly();
+	bcf->CreateInit(pos, direction, scene);
+	dynamic_cast<SceneDev3*>(scene)->AddMonster(bcf);
+	//dynamic_cast<SceneGame*>(scene)->AddMonster(bcf);
+	return bcf;
 }
 
 void BulletCroaksFirefly::Update(float dt)
 {
 	ObjectBullet::Update(dt);
+	customBounds = sprite.getGlobalBounds();
 }
 
 void BulletCroaksFirefly::Init()
 {
 	ObjectBullet::Init();
+	owner = Owner::Enemy;
+	SetTexture("resource/CroaksFireFly.png");
+	SetSpeed(600.f);
+	SetRange(1200.f);
+	type = Type::Homing;
 }
 
 void BulletCroaksFirefly::OnDie()
